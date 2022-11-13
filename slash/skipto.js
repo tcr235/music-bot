3500a92a0627a9bf110571a938db0963562fb93b
Thiago Conte <tcr2@cin.ufpe.br>
2022-11-13 19:52:21 -0300

    Add skipto command

---
+++ slash/skipto.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("skipto")
+        .setDescription("Pula para uma música específica")
+        .addNumberOption((option) => 
+            option.setName("tracknumber").setDescription("Número da música que vai tocar").setminimumValue(1).setRequired(true)
+        ), 
+
+    run: async ({ client, interaction }) => {
+        const queue = client.player.getQueue(interaction.guildId)
+
+        if (!queue) return await interaction.editReply("Não há músicas na fila")
+        
+        const trackNum = interaction.options.getNumber("tracknumber")
+        if (trackNum > queue.tracks.lenght)
+            return await interaction.editReply("Número inválido")
+        queue.skipTo(trackNum - 1)
+        await interaction.editReply(`Passado para a música número ${trackNum}`)
+        
+    }
+
+}
