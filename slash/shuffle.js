515636134ed186ea0db98998e3551434c7a73cae
Thiago Conte <tcr2@cin.ufpe.br>
2022-11-13 19:04:12 -0300

    Add shuffle command

--- slash/shuffle.js
+++ slash/shuffle.js
@@ -2,16 +2,16 @@ const { SlashCommandBuilder } = require('@discordjs/builders')
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("quit")
-        .setDescription("Retira o bot e limpa a fila"), 
+        .setName("shuffle")
+        .setDescription("Mistura a ordem da fila"), 
 
     run: async ({ client, interaction }) => {
         const queue = client.player.getQueue(interaction.guildId)
 
         if (!queue) return await interaction.editReply("Não há músicas na fila")
         
-        queue.destroy()
-        await interaction.editReply("Até mais!")
+        queue.shuffle()
+        await interaction.editReply(`A fila contendo ${queue.tracks.length} músicas foi reordenada!`)
         
     }
 
