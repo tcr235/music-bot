2684b287eea1483e5883e6fe25e4d27b74072b95
Thiago Conte <tcr2@cin.ufpe.br>
2022-11-13 17:14:08 -0300

    Deploy de comandos slash

--- index.js
+++ index.js
@@ -28,3 +28,28 @@ client.player = new Player(client, {
         highWaterMark: 1 << 25
     }
 });
+
+let commands = []
+
+const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
+for (const file of slashFiles){
+    const slashcmd = require(`./slash/${file}`)
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+}
+
+if (LOAD_SLASH) {
+    const rest = new REST({ version: "9" }).setToken(TOKEN)
+    console.log("Adicionando comandos slash")
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID, {body: commands}))
+    .then(() => {
+        console.log("Carregado com sucesso")
+        process.exit(0)
+    })
+    .catch((err) => {
+        if (err){
+            console.log("Erro")
+            process.exit(1)
+        }
+    })
+}
