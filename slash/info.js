80ff96fecb7b2b1f5b8a4a0ccbf92617c1d9d2bf
Thiago Conte <tcr2@cin.ufpe.br>
2022-11-13 19:16:40 -0300

    Add info command

---
+++ slash/info.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("info")
+        .setDescription("Mostra informações sobre a música atual"), 
+
+    run: async ({ client, interaction }) => {
+        const queue = client.player.getQueue(interaction.guildId)
+
+        if (!queue) return await interaction.editReply("Não há músicas na fila")
+        
+        let bar = queue.createProgressBar({
+            queue: false,
+            length: 19
+        })
+
+        const song = queue.current
+
+        await interaction.editReply({
+            embeds: [new MessageEmbed()
+            .setThumbnail(song.thumbnail)
+            .setDescription(`Reproduzindo [${song.title}](${song.url})\n\n` + bar)
+        ],
+        })
+        
+    }
+
+}
